0fb2c4faacf59ad12003ccb56639c2288036394e
Mario <mariovidovic97@gmail.com>
2023-01-14 01:22:54 +0100

    singlica za knjigu

--- src/views/BookView.vue
+++ src/views/BookView.vue
@@ -1,15 +1,18 @@
 <template>
-    <section class="book">
-        <div class="wrapper">
-            <p>The ID is: {{ id }}</p>
-              <ul>
-      <li v-for="item in filteredItems" :key="item.id">
-        <p>{{ item.name }}</p>
-    <p>{{ item.desc }}</p>     
-    </li>
-    </ul>
-        </div>
-    </section>
+  <a href="/katalog" class="book-back">Popis knjiga</a>
+  <section class="book-row">
+    <div class="wrapper">
+      <img v-for="item in filteredItems" :key="item.id" v-bind:src="'/'+item.cover" v-bind:alt="item.name" data-aos="fade-up" data-aos-duration="800" />
+      <div class="book-row-info" v-for="item in filteredItems" :key="item.id">
+        <p class="book-name">{{item.name}}</p>
+        <p class="book-author">{{item.author}}</p>
+        <span v-for="n in 5" :key="n" class="fa fa-star stars" :class="{ filled: n <= item.ocjena }"></span>
+        <p class="book-desc">{{item.desc}}</p>
+      </div>
+    </div>
+  </section>
+  <section class="book">
+  </section>
 </template>
   
 <script>
@@ -22,17 +25,15 @@ export default {
       items: [],
     };
   },
-  
   created() {
-     this.id = this.$route.params.id;
-    axios.get('/books.json')
-      .then(response => {
-        this.items = response.data;
-      });
+    this.id = "knjiga/" + this.$route.params.id;
+    axios.get('/books.json').then(response => {
+      this.items = response.data;
+    });
   },
   computed: {
     filteredItems() {
-      return this.items.filter(item => item.url == this.id);
+      return this.items.filter(item => item.url === this.id);
     }
   },
 };
