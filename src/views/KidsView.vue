9ce3307109a0e112718a785be8846e8d3f05de9a
Mario <mariovidovic97@gmail.com>
2023-02-01 19:43:59 +0100

    sadržaj za odjele

--- src/views/KidsView.vue
+++ src/views/KidsView.vue
@@ -12,13 +12,27 @@
     <section class="department">
         <div class="wrapper">
             <h1>Dječji odjel</h1>
+            <div class="wrapper-text">
+                <img src="@/assets/odjel-djeca.jpg" />
+                <p>Odjel za odrasle nalazi se u prizemlju knjižnice i namijenjen je korisnicima starijim od 14 godina, ali vrlo rado primamo i naše najmlađe članove koji koriste usluge <a href="/odjeli/djecji">Dječjeg odjela</a>. Po broju članova, po bogatstvu knjižnog fonda i po veličini prostora Odjel za odrasle je najveći odjel knjižnice Codex.</p>
+                <p>U svojem otvorenom prostoru na prvom katu, Odjel za odrasle ima više od 54 000 knjiga, DVD-a, stripova. S obzirom na to da naša Knjižnica, osim uloge narodne knjižnice, ima i stručno-znanstveni karakter, uz mnogobrojnu i raznovrsnu beletrističku literaturu, posjeduje i velik broj znanstvenih i stručnih publikacija te knjiga na stranim jezicima.</p>
+                <p>Prostor Odjela za odrasle opremljen je mjestima prikladnima za odmor, instrukcije, individualni ili grupni rad koji ne zahtijeva strogo propisanu tišinu. Također, prostor Odjela korisnicima nudi na raspolaganje tri računala za pisanje i pristup internetu, pisač s mogućnostima kopiranja, printanja i skeniranja, a dostupna je i besplatna Wi-Fi mreža. Korisnici mogu sami ili uz pomoć stručnog osoblja pregledavati i pretraživati bogatu zbirku audiovizualne građe koja uključuje igrane i dokumentarne filmove, serije i koncerte.</p>
+                <p>Informacijska služba Odjela za odrasle pronalazi literaturu za referate, seminarske, maturalne i završne radove, pretražuje vlastite baze podataka te izrađuje tematske ispise literature na traženu temu, poučava korisnike samostalnom korištenju kataloga i fonda, savjetuje korisnike pri izboru knjiga, izrađuje biltene prinovljenih knjiga, priređuje tematske izložbe i organizira međuknjižničnu posudbu knjiga.</p>
+            </div>
         </div>
     </section>
+
+    <Footer></Footer>
 </template>
   
 <script>
+import Footer from '@/components/Footer.vue';
+
 export default {
     name: 'KidsView',
+    components: {
+        Footer,
+    },
     data() {
         return {};
     },
--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -12,13 +12,26 @@
     <section class="department">
         <div class="wrapper">
             <h1>Glazbena zbirka</h1>
+            <div class="wrapper-text">
+                <img src="@/assets/odjel-glazba.jpg" />
+                <p>Ovaj odjel s oko 50.000 jedinica audiovizualne i notne građe nudi članovima kompaktne zvučne ploče (CD), DVD, zvučne i videokasete, gramofonske ploče, note, glazbene časopise, knjige i priručnike. AV građa se može posuditi na rok od 10 dana, a note i knjige na 21 dan i/ili koristiti u knjižnici. Zastupljena su sva područja glazbe, od narodne, umjetničke i jazz glazbe do pop i rock, dance glazbe i dr.</p>
+                <p>Glazbeni odjel priređuje <b>koncerte ili glazbene promocije</b>.</p>
+                <p>Glazbeni odjel ima ulogu matičnog odjela. Aktivno sudjeluje u organiziranju i savjetovanju u razvoju drugih audiovizualnih zbirki, edukaciji djelatnika za rad u takvim zbirkama i odjelima. Na redovitim sastancima koordinira nabavu i obradu audiovizualne građe glazbenog sadržaja ostalim odjelima. Kreira i provodi razne oblike kulturnih, obrazovnih, edukativnih i ostalih aktivnosti (prigodni koncerti, promocije, tematske izložbe, radionice, suradnja s brojnim glazbenim ustanovama, dobrotvornim i profesionalnim udruženjima).</p>
+            </div>
         </div>
     </section>
+
+    <Footer></Footer>
 </template>
   
 <script>
+import Footer from '@/components/Footer.vue';
+
 export default {
     name: 'MusicView',
+    components: {
+        Footer,
+    },
     data() {
         return {};
     },
